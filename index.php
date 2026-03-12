f3fd218f02001333a046db8551344b2d0c47066e
Zeran Lu <zeranlu0326@gmail.com>
2026-03-11 20:24:03 -0400

    LV2: Created testing index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Filmography\HorrorMovies;
+
+$title = 'something';
+
+$movie1 = new HorrorMovies();
+$movie1->title = 'something';
+
+$movie2 = new HorrorMovies;
+$movie2->title = 'something else';
+
+$title = 'something else';
+
