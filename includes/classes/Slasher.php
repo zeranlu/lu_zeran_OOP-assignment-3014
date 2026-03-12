a2c0397ff3cd6c2f545aa31b895b3ccc1c9a1dba
Zeran Lu <zeranlu0326@gmail.com>
2026-03-11 20:27:43 -0400

    LV2: added example override function to Slasher.php

--- includes/classes/Slasher.php
+++ includes/classes/Slasher.php
@@ -4,5 +4,8 @@ namespace Filmography;
 
 class Slasher extends HorrorMovies
 {
-    
+    public function getDescription()
+    {
+        return parent::getDescription() . 'Most of these slashers have an iconic killer! Look out for ' . $this->villain . ' on the big screen!';
+    }
 }
