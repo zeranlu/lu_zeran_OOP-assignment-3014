133f34ede51c93735ca223c7deb0b12b78170233
Zeran Lu <zeranlu0326@gmail.com>
2026-03-13 14:53:44 -0400

    INDEX: added Scream HTML autoload content

--- index.php
+++ index.php
@@ -11,17 +11,6 @@ spl_autoload_register(function ($class) {
     }
 });
 
-// TESTING AUTOLOAD
-
-// $slasher = new Filmography\Slasher();
-// $slasher->title = 'Scream';
-// echo $slasher->title;
-
-// $alien = new Filmography\Extraterrestrial();
-// $alien->title = 'Alien';
-// $alien->director = 'Ridley Scott';
-// echo $alien->title . ' directed by ' . $alien->director;
-
 // HIERARCHY EXAMPLES:
 
 // defining variables at the level 1 (HorrorMovies.php)
@@ -31,14 +20,71 @@ $slasher->director = 'Wes Craven';
 $slasher->franchise = 'Scream Franchise';
 $slasher->installments = '7';
 $slasher->villain = 'Ghostface';
+$slasher->reason = 'this is an classic serires with an iconic killer that marries comedy and horror.';
+$slasher->recommendation = 'Happy Death Day';
 
-echo
-    // functions exists in HorroMovies.php
-    $slasher->getDescription()
-    .$slasher->getVillain()
-    // function exists under the PersonalFavourite.php trait file
-    .$slasher->getRating()
-    // functions exist in the Slasher.php (genre) class
-    .$slasher->getIconDescription()
-    .$slasher->getKillerWarning();
+// ECHO EXAMPLE:
 
+// echo
+
+//     // functions exists in HorroMovies.php
+//     $slasher->getDescription()
+//     .$slasher->getVillain()
+
+//     // function exists under the PersonalFavourite.php trait file
+//     .$slasher->getRating()
+
+//     // functions exist in the Slasher.php (genre) class
+//     .$slasher->getIconDescription()
+//     .$slasher->getKillerWarning();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horror Movie Catalogue</title>
+</head>
+<body>
+    <header>
+        <h1>Check out all these horror movies!</h1>
+    </header>
+
+    <main>
+        <div id="scream-con">
+            <h2>Scream</h2>
+
+            <h3>These are methods of HorrorMovies.php</h3>
+            <p>
+                <?php echo $slasher->getDescription();?>
+            </p>
+
+            <p>
+                <?php echo $slasher->getVillain();?>
+            </p>
+
+            <h3>These are methods of my trait PersonalFavourite.php</h3>
+            <p>
+                <?php echo $slasher->getRating();?>
+            </p>
+
+            <p>
+                <?php echo $slasher->getReason();?>
+            </p>
+
+            <h3>These are methods of my subclass Slasher.php</h3>
+            <p>
+                <?php echo $slasher->getIconDescription();?>
+            </p>
+
+            <p>
+                <?php echo $slasher->getKillerWarning();?>
+            </p>
+        </div>
+
+
+    </main>
+</body>
+</html>
